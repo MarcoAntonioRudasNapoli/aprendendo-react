7e34ccedae0f0bd1f36d5baf527f1205c9c48f49
MarcoAntonioRudasNapoli <marco.rudas@polijunior.com.br>
2021-03-01 10:57:33 -0300

    update: Aula 4: Componetização

--- Introdução ao React/projeto/src/index.js
+++ Introdução ao React/projeto/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import App from './App';
 
+//JSX: HTML dentro do JavaScript (JavaScript XML)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <App />,
   document.getElementById('root')
 );
+
