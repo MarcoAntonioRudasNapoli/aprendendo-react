8acb20de187dcf4e4edecaeb868d91dfc340abef
MarcoAntonioRudasNapoli <marco.rudas@polijunior.com.br>
2021-03-01 11:43:54 -0300

    update: Aula 6: Estado e Imutabilidade

--- Introdução ao React/projeto/src/App.js
+++ Introdução ao React/projeto/src/App.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './components/Header'
 /*
   * Componente
   * Propriedade
-  * Estado
+  * Estado e Imutabilidade
 */
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  /*
+    useState retorna um array com duas posições
+    1. VAriável com o seu  valor inicial
+    2. Função para atualizarmos eesse valor
+  */
+
+  function handleAddProject(){
+    //projects.push(`Novo projeto ${Date.now()}`);
+    
+    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  }
+
   return (
   <>
-    <Header title="Homepage">
-      <ul>
-        <li>Homepage</li>
-        <li>Projects</li>
-      </ul>
-    </Header>
-    <Header title="Projects">
+    <Header title="Projects"/>
     <ul>
-        <li>Homepage</li>
-        <li>Projects</li>
-        <li>Login</li>
-      </ul>
-    </Header>
+      {projects.map(project => <li key={project}>{project}</li>)}
+    </ul>
+
+    <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
   </>
   );
 }
